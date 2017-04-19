aba3ae1f4e885d7c601dabd5f2d5ce6cf353cea5
guitarrespieler <tibor.zsiga@hotmail.com>
2017-04-19 23:46:40 +0200

    First commit

---
+++ messenger.backend/src/main/java/iit/bme/hu/messenger/backend/App.java
@@ -0,0 +1,13 @@
+package iit.bme.hu.messenger.backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ messenger.backend/src/main/java/iit/bme/hu/messenger/backend/model/User.java
@@ -0,0 +1,10 @@
+package iit.bme.hu.messenger.backend.model;
+
+/**
+ * Represents a user. DTO class.
+ * @author zsigatibor
+ *
+ */
+public class User {
+
+}
