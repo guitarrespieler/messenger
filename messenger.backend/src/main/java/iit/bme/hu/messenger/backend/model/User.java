5f012273ffb6f51ce84251a77ecdfeb1b15d2533
guitarrespieler <tibor.zsiga@hotmail.com>
2017-04-20 00:24:50 +0200

    More basic things

--- messenger.backend/src/main/java/iit/bme/hu/messenger/backend/model/User.java
+++ messenger.backend/src/main/java/iit/bme/hu/messenger/backend/model/User.java
@@ -1,10 +1,24 @@
 package iit.bme.hu.messenger.backend.model;
 
+import java.util.Date;
+import java.util.List;
+
+import iit.bme.hu.messenger.backend.model.enums.Role;
+
 /**
  * Represents a user. DTO class.
  * @author zsigatibor
  *
  */
 public class User {
-
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;	
+	private String placeOfBirth;
+	private Date birthDate;
+	private List<User> friends;
+	private List<Conversation> conversations;
+	private Role role;
+	
 }
